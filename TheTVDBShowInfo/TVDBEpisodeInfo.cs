5cd3b3d1678158124298cdc66c83bfe7fa229e76
Dan Esparza <esparza.dan@gmail.com>
2013-10-11 01:07:37 -0400

    Added meta classes for TVDB episode information

---
+++ TheTVDBShowInfo/TVDBEpisodeInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TheTVDBShowInfo
+{
+    public class TVDBEpisodeInfo
+    {
+        public string EpisodeNumber { get; set; }
+        public string SeasonNumber { get; set; }
+
+        [XmlElement(ElementName = "FirstAired")]
+        public DateTime OriginalAirDate { get; set; }
+
+        public string EpisodeName { get; set; }
+
+        [XmlElement(ElementName = "Overview")]
+        public string EpisodeSummary { get; set; }
+    }
+
+    [XmlRoot("Data")]
+    public class TVDBEpisodeResult
+    {
+        [XmlElement(ElementName = "Episode")]
+        public TVDBEpisodeInfo EpisodeInfo { get; set; }
+    }
+}
